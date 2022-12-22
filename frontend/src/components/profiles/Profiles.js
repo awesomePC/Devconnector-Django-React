eb09115d7648ae6fe317bf9c8a03ca3702dd9b91
awesome <billycrow268@gmail.com>
2022-12-21 19:06:22 -0800

    frontend interface css

---
+++ frontend/src/components/profiles/Profiles.js
@@ -0,0 +1,22 @@
+import React, { Component } from "react";
+import { useSelector } from "react-redux";
+import { Route, Redirect } from "react-router-dom";
+
+const PrivateRoute = ({component: Component, ...rest}) => {
+    const auth = useSelector(state => state.auth);
+    const { loading, isAuthenticated } = auth;
+
+    return (
+        <Route
+            {...rest}
+            render={props => !isAuthenticated && !loading ? (
+                    <Redirect to="/login" />
+                ):(
+                    <Component {...props}/>
+                )
+            }
+        />
+    );
+};
+
+export default PrivateRoute;
